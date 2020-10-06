91c4be0528af3111e21965e6a019419e632e5302
furkan.yalcindag@turkai.com <Furkan1905>
2020-10-06 17:25:52 +0300

    Customer Component

--- src/views/customer/Customer.vue
+++ src/views/customer/Customer.vue
@@ -5,26 +5,26 @@
         <transition name="fade">
           <CCard v-if="show">
             <CCardHeader>
-              <CIcon name="cil-pencil" />
+              <CIcon name="cil-pencil"/>
               Müşteri
               <div class="card-header-actions">
                 <CLink href="#" class="card-header-action btn-setting">
-                  <CIcon name="cil-settings" />
+                  <CIcon name="cil-settings"/>
                 </CLink>
                 <CLink
-                  class="card-header-action btn-minimize"
-                  @click="formCollapsed = !formCollapsed"
+                    class="card-header-action btn-minimize"
+                    @click="formCollapsed = !formCollapsed"
                 >
                   <CIcon
-                    :name="`cil-chevron-${formCollapsed ? 'bottom' : 'top'}`"
+                      :name="`cil-chevron-${formCollapsed ? 'bottom' : 'top'}`"
                   />
                 </CLink>
                 <CLink
-                  href="#"
-                  class="card-header-action btn-close"
-                  v-on:click="show = !show"
+                    href="#"
+                    class="card-header-action btn-close"
+                    v-on:click="show = !show"
                 >
-                  <CIcon name="cil-x-circle" />
+                  <CIcon name="cil-x-circle"/>
                 </CLink>
               </div>
             </CCardHeader>
@@ -36,10 +36,10 @@
                   </CAlert>
 
                   <CAlert
-                    v-for="item in errors"
-                    :key="item.message"
-                    color="danger"
-                    :show="isError"
+                      v-for="item in errors"
+                      :key="item.message"
+                      color="danger"
+                      :show="isError"
                   >
                     E-mail: {{ item }}
                   </CAlert>
@@ -48,42 +48,42 @@
                 <CRow>
                   <CCol lg="3">
                     <CInput
-                      label="İsim"
-                      description=""
-                      autocomplete="autocomplete"
-                      v-model="customer.firstName"
+                        label="İsim"
+                        description=""
+                        autocomplete="autocomplete"
+                        v-model="customer.firstName"
                     />
 
                     <CInput
-                      label="Soyisim"
-                      description=""
-                      autocomplete="autocomplete"
-                      v-model="customer.lastName"
+                        label="Soyisim"
+                        description=""
+                        autocomplete="autocomplete"
+                        v-model="customer.lastName"
                     />
 
                     <CInput
-                      label="Email"
-                      description=""
-                      type="email"
-                      autocomplete="email"
-                      prepend="@"
-                      v-model="customer.username"
+                        label="Email"
+                        description=""
+                        type="email"
+                        autocomplete="email"
+                        prepend="@"
+                        v-model="customer.username"
                     />
                   </CCol>
 
                   <CCol lg="3">
                     <CInput
-                      label="Telefon Numarası"
-                      description=""
-                      autocomplete="autocomplete"
-                      v-model="customer.mobilePhone"
+                        label="Telefon Numarası"
+                        description=""
+                        autocomplete="autocomplete"
+                        v-model="customer.mobilePhone"
                     />
                     <CTextarea
-                      :rows="3"
-                      label="Adres"
-                      description=""
-                      autocomplete="autocomplete"
-                      v-model="customer.address"
+                        :rows="3"
+                        label="Adres"
+                        description=""
+                        autocomplete="autocomplete"
+                        v-model="customer.address"
                     />
 
                     <CRow form class="form-group">
@@ -93,10 +93,10 @@
 
                       <CCol sm="8" :class="'form-inline'">
                         <CInputCheckbox
-                          :label="'evet'"
-                          :value="'true'"
-                          :checked="isCorporate"
-                          v-on:change="isCorporateControl"
+                            :label="'evet'"
+                            :value="'true'"
+                            :checked="isCorporate"
+                            v-on:change="isCorporateControl"
                         />
                       </CCol>
                     </CRow>
@@ -104,24 +104,24 @@
 
                   <CCol lg="3">
                     <CInput
-                      label="Firma Adı"
-                      description=""
-                      autocomplete="autocomplete"
-                      v-model="customer.firmName"
+                        label="Firma Adı"
+                        description=""
+                        autocomplete="autocomplete"
+                        v-model="customer.firmName"
                     />
 
                     <CInput
-                      label="Vergi Numarası"
-                      description=""
-                      autocomplete="autocomplete"
-                      v-model="customer.taxNumber"
+                        label="Vergi Numarası"
+                        description=""
+                        autocomplete="autocomplete"
+                        v-model="customer.taxNumber"
                     />
                   </CCol>
                 </CRow>
 
                 <div class="form-actions">
                   <CButton type="submit" color="primary" @click="addCustomer"
-                    >Save changes
+                  >Save changes
                   </CButton>
                   <CButton color="secondary">Cancel</CButton>
                 </div>
@@ -131,96 +131,50 @@
         </transition>
       </CCol>
     </CRow>
-    <CRow>
-      <CCol lg="12">
-        <transition name="fade">
-          <CCard v-if="show">
-            <CCardBody>
-              <CDataTable
-                :items="items"
-                :fields="fieldsTable"
-                column-filter
-                table-filter
-                items-per-page-select
-                :items-per-page="5"
-                :activePage="4"
-                hover
-                sorter
-                pagination
-              >
-                <template #status="{ item }">
-                  <td>
-                    <CBadge :color="getBadge(item.status)">
-                      {{ item.status }}
-                    </CBadge>
-                  </td>
-                </template>
-                <template #show_details="{ item, index }">
-                  <td class="py-2">
-                    <CButton
-                      color="primary"
-                      variant="outline"
-                      square
-                      size="sm"
-                      @click="toggleDetails(item, index)"
-                    >
-                      {{ Boolean(item._toggled) ? "Hide" : "Show" }}
-                    </CButton>
-                  </td>
-                </template>
-                <template #details="{ item }">
-                  <CCollapse
-                    :show="Boolean(item._toggled)"
-                    :duration="collapseDuration"
-                  >
-                    <CCardBody>
-                      <CMedia :aside-image-props="{ height: 102 }">
-                        <h4>
-                          {{ item.username }}
-                        </h4>
-                        <p class="text-muted">
-                          User since: {{ item.registered }}
-                        </p>
-                        <CButton size="sm" color="info" class="">
-                          User Settings
-                        </CButton>
-                        <CButton size="sm" color="danger" class="ml-1">
-                          Delete
-                        </CButton>
-                      </CMedia>
-                    </CCardBody>
-                  </CCollapse>
-                </template>
-              </CDataTable>
-            </CCardBody>
-          </CCard>
-        </transition>
-      </CCol>
-    </CRow>
+
     <CRow>
       <CCol lg="12">
         <transition name="fade">
           <CCard v-if="show">
             <CCardBody>
               <template>
-    <v-card>
-    <v-card-title>
-      Nutrition
-      <v-spacer></v-spacer>
-      <v-text-field
-        v-model="search"
-        append-icon="mdi-magnify"
-        label="Search"
-        single-line
-        hide-details
-      ></v-text-field>
-    </v-card-title>
-    <v-data-table
-      :headers="headers"
-      :items="desserts"
-      :search="search"
-    ></v-data-table>
-  </v-card>
+                <v-card>
+                  <v-card-title>
+                    Nutrition
+                    <v-spacer></v-spacer>
+                    <v-text-field
+                        v-model="search"
+                        append-icon="mdi-magnify"
+                        label="Search"
+                        single-line
+                        hide-details
+                    ></v-text-field>
+                  </v-card-title>
+                  <template>
+                  <v-data-table
+                      :headers="headers"
+                      :items="customer"
+                      :search="search"
+                      :items-per-page="10"
+                      item-key="name"
+                      class="elevation-1"
+                      :total-items="total"
+                      :loading="loading"
+
+
+                  >
+
+                    <template v-slot:item="{ item }">
+                      <CButtonGroup>
+                        <CButton color="success">Button</CButton>
+                        <CButton color="info">Button</CButton>
+                        <CButton color="primary">Button</CButton>
+                      </CButtonGroup>
+                    </template>
+
+                  </v-data-table>
+                  </template>
+                </v-card>
               </template>
             </CCardBody>
           </CCard>
@@ -234,187 +188,29 @@
 import Customer from "../../models/customer";
 import CustomerService from "@/services/customer.service";
 import Vuetify from "vuetify/lib";
+import * as axios from "core-js";
 
-const items = [
-  {
-    username: "Samppa Nori",
-    registered: "2012/01/01",
-    role: "Member",
-    status: "Active",
-  },
-  {
-    username: "Estavan Lykos",
-    registered: "2012/02/01",
-    role: "Staff",
-    status: "Banned",
-  },
-  {
-    username: "Chetan Mohamed",
-    registered: "2012/02/01",
-    role: "Admin",
-    status: "Inactive",
-  },
-  {
-    username: "Derick Maximinus",
-    registered: "2012/03/01",
-    role: "Member",
-    status: "Pending",
-  },
-  {
-    username: "Friderik Dávid",
-    registered: "2012/01/21",
-    role: "Staff",
-    status: "Active",
-  },
-  {
-    username: "Yiorgos Avraamu",
-    registered: "2012/01/01",
-    role: "Member",
-    status: "Active",
-  },
-  {
-    username: "Avram Tarasios",
-    registered: "2012/02/01",
-    role: "Staff",
-    status: "Banned",
-    _classes: "table-success",
-  },
-  {
-    username: "Quintin Ed",
-    registered: "2012/02/01",
-    role: "Admin",
-    status: "Inactive",
-  },
-  {
-    username: "Enéas Kwadwo",
-    registered: "2012/03/01",
-    role: "Member",
-    status: "Pending",
-  },
-  {
-    username: "Agapetus Tadeáš",
-    registered: "2012/01/21",
-    role: "Staff",
-    status: "Active",
-  },
-  {
-    username: "Carwyn Fachtna",
-    registered: "2012/01/01",
-    role: "Member",
-    status: "Active",
-    _classes: "table-info",
-  },
-  {
-    username: "Nehemiah Tatius",
-    registered: "2012/02/01",
-    role: "Staff",
-    status: "Banned",
-  },
-  {
-    username: "Ebbe Gemariah",
-    registered: "2012/02/01",
-    role: "Admin",
-    status: "Inactive",
-  },
-  {
-    username: "Eustorgios Amulius",
-    registered: "2012/03/01",
-    role: "Member",
-    status: "Pending",
-  },
-  {
-    username: "Leopold Gáspár",
-    registered: "2012/01/21",
-    role: "Staff",
-    status: "Active",
-  },
-  {
-    username: "Pompeius René",
-    registered: "2012/01/01",
-    role: "Member",
-    status: "Active",
-  },
-  {
-    username: "Paĉjo Jadon",
-    registered: "2012/02/01",
-    role: "Staff",
-    status: "Banned",
-  },
-  {
-    username: "Micheal Mercurius",
-    registered: "2012/02/01",
-    role: "Admin",
-    status: "Inactive",
-  },
-  {
-    username: "Ganesha Dubhghall",
-    registered: "2012/03/01",
-    role: "Member",
-    status: "Pending",
-  },
-  {
-    username: "Hiroto Šimun",
-    registered: "2012/01/21",
-    role: "Staff",
-    status: "Active",
-  },
-  {
-    username: "Vishnu Serghei",
-    registered: "2012/01/01",
-    role: "Member",
-    status: "Active",
-  },
-  {
-    username: "Zbyněk Phoibos",
-    registered: "2012/02/01",
-    role: "Staff",
-    status: "Banned",
-  },
-  {
-    username: "Einar Randall",
-    registered: "2012/02/01",
-    role: "Admin",
-    status: "Inactive",
-    _classes: "table-danger",
-  },
-  {
-    username: "Félix Troels",
-    registered: "2012/03/21",
-    role: "Staff",
-    status: "Active",
-  },
-  {
-    username: "Aulus Agmundr",
-    registered: "2012/01/01",
-    role: "Member",
-    status: "Pending",
-  },
-];
+
 
 const fields = [
-  { key: "Müşteri", _style: "min-width:200px" },
-  { key: "Telefon Numarası" },
+  {key: "Müşteri", _style: "min-width:200px"},
+  {key: "Telefon Numarası"},
 ];
 
-const fieldsTable = [
-  { key: "username", _style: "min-width:200px" },
-  "registered",
-  { key: "role", _style: "min-width:100px;" },
-  { key: "status", _style: "min-width:100px;" },
-  {
-    key: "show_details",
-    label: "",
-    _style: "width:1%",
-    sorter: false,
-    filter: false,
-  },
-];
+
 
 export default {
   name: "Customer",
   vuetify: new Vuetify(),
   data() {
     return {
+
+      selected: [],
+      rowsPerPageItems: [10],
+      search: '',
+      total: 0,
+      loading: false,
+      pagination: {},
       desserts: [
         {
           name: "Frozen Yogurt",
@@ -423,6 +219,7 @@ export default {
           carbs: 24,
           protein: 4.0,
           iron: "1%",
+          actions:true
         },
         {
           name: "Ice cream sandwich",
@@ -502,28 +299,24 @@ export default {
           text: "Dessert (100g serving)",
           align: "start",
           sortable: false,
-          value: "name",
+          value: "gender",
         },
-        { text: "Calories", value: "calories" },
-        { text: "Fat (g)", value: "fat" },
-        { text: "Carbs (g)", value: "carbs" },
-        { text: "Protein (g)", value: "protein" },
-        { text: "Iron (%)", value: "iron" },
+        {text: "Tax", value: "tax_number"},
+
+        {text: "Actions (%)", value: "actions"}
       ],
       customer: new Customer("", "", "", "", "", "", "", ""),
-      items: items.map((item, id) => {
-        return { ...item, id };
-      }),
+
       isSuccess: false,
       isError: false,
-      fieldsTable,
+
       details: [],
       errors: [],
       isCorporate: false,
       collapseDuration: 0,
-      selected: [], // Must be an array reference!
+
       show: true,
-      horizontal: { label: "col-3", input: "col-9" },
+      horizontal: {label: "col-3", input: "col-9"},
       options: ["Option 1", "Option 2", "Option 3"],
       selectOptions: [
         "Option 1",
@@ -615,10 +408,38 @@ export default {
       setTimeout(() => (this.isSuccess = false), 5000);
       console.log("naber");
     },
+
+    getCategoriesByPagination() {
+      this.loading = true;
+      // get by search keyword
+
+
+      if (this.search) {
+        axios.get(`localhost:8000/car-service/customer-api/?search=${this.search}&page=${this.pagination.page}&per_page=${this.pagination.rowsPerPage}`)
+            .then(res => {
+              this.customer = res.data.data;
+              this.total = res.data.recordsTotal;
+            })
+            .catch(err => console.log(err.response.data))
+            .finally(() => this.loading = false);
+      }
+    }
+
+
+  },
+  watch: {
+    pagination() {
+      this.getCategoriesByPagination();
+    },
+    search() {
+      this.getCategoriesByPagination();
+    }
   },
   created() {
     this.isCorporateControl();
+    this.getCategoriesByPagination();
   },
-  mounted() {},
+  mounted() {
+  },
 };
 </script>
