73762785695c7f16361cbd16ab1f2c584ecaf845
Furkan Yalçındağ <fyalcindag@oxityazilim.com>
2021-02-28 00:21:15 +0300

    customer delete

--- src/services/customer.service.js
+++ src/services/customer.service.js
@@ -47,6 +47,33 @@ class CustomerService {
     }
 
 
+    deleteCustomer(id) {
+        const params = {
+            id: id
+
+        };
+        return axios.delete(process.env.VUE_APP_API_URL + `/car-service/customer-api/`, {
+            headers: authHeader(),
+            params
+        }).then(response => {
+            console.log(response)
+            return response;
+        }).catch((err) => {
+            if (err.response) {
+                console.log("resp", err.response)
+                return err.response
+            } else if (err.request) {
+                // client never received a response, or request never left
+                console.log("req", err.request)
+            } else {
+                // anything else
+            }
+        })
+
+
+    }
+
+
 }
 
 export default CustomerService
